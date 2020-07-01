6fe70fa60ba1d995b3be4bdcc0b498e81009f150
rubone <rubone02@gmail.com>
2020-06-30 22:11:12 -0600

    :construction: Agregando clase para ThemeProvider

---
+++ flutter_theme/lib/providers/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:fluttertheme/models/theme_preferences.dart';
+
+class ThemeProvider with ChangeNotifier{
+  ThemePreferences themePreferences = ThemePreferences();
+  String _theme = ThemePreferences.LIGHT;
+
+  String get theme => _theme;
+
+  set setTheme(String theme){
+    _theme = theme;
+    themePreferences.setModeTheme(theme);
+    notifyListeners();
+  }
+
+}
