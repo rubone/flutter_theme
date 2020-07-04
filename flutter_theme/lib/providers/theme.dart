7e330fa329769847a71039431b5fcff95361d2aa
rubone <rubone02@gmail.com>
2020-07-03 22:09:39 -0600

    :construction: Agregando funcion para validar tema

--- flutter_theme/lib/providers/theme.dart
+++ flutter_theme/lib/providers/theme.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:fluttertheme/models/theme_preferences.dart';
 
 class ThemeProvider with ChangeNotifier{
@@ -12,5 +12,7 @@ class ThemeProvider with ChangeNotifier{
     themePreferences.setModeTheme(theme);
     notifyListeners();
   }
+
+  bool isDarkTheme () => _theme == ThemePreferences.DARK;
 
 }
